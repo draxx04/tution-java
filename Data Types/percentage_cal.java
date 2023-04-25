0de148d63559af9f5737dbce6417d426f69da2f5
Kalyan <kalyanray2004@gmail.com>
2023-04-25 14:36:51 +0530

    added more javas

--- Data Types/percentage_cal.java
+++ Data Types/percentage_cal.java
@@ -12,10 +12,15 @@
 class percentage_cal {
     public static void main(String[] args) {
         double total_literacy_rate = 48; // % of total literacy rate
-        double total_illiterate_rate = 100 - total_literacy_rate;
+        double total_literate_men = 35; // 35%
+        double total_illiterate_rate = 100 - total_literacy_rate; // 100% - 48% = 52%
         double total_population = 80000;
-        double total_illiteracy_rate_of_men_women = (total_population * total_illiterate_rate) / 100;
-        System.out.println("total illiterate men and women are : " + total_illiteracy_rate_of_men_women);
+        double total_illiteracy_rate_of_men_women = (total_population * total_illiterate_rate) / 100; // 80000*52%
+        double total_illiterate_men = (total_illiteracy_rate_of_men_women * total_literate_men) / 100; // 41600 * 35/100
+        double total_illiterate_women = total_illiteracy_rate_of_men_women - total_illiterate_men;
+        System.out.println("total illiterate men : " + total_illiterate_men);
+        System.out.println("total illiterate women :" + total_illiterate_women);
+        System.out.println(" The total of illiterate men and women are : " + total_illiteracy_rate_of_men_women);
 
     }
 }
