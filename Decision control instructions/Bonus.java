e2eb6bae451575021a126bf6ad41b13fb3ef17cf
Kalyan <kalyanray2004@gmail.com>
2023-04-27 19:26:53 +0530

    Add more javas

---
+++ Decision control instructions/Bonus.java
@@ -0,0 +1,14 @@
+//Calculation of Bonus of Employee
+import java.util.Scanner;
+class Bonus{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the current year : ");
+        int cur_year = sc.nextInt();
+        System.out.println("enter the year in which the employee joined : ");
+        int yr_of_join = sc.nextInt();
+        sc.close();
+        
+
+    }
+}
