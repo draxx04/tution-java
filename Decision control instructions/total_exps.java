7b764c26261bbde235048e8b2f8a8b033ffdf24f
Kalyan <kalyanray2004@gmail.com>
2023-04-27 17:21:42 +0530

    Add more javas

--- Decision control instructions/total_exps.java
+++ Decision control instructions/total_exps.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class total_exp {
+public class total_exps {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the quantity of item : ");
