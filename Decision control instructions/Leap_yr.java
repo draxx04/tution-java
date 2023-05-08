c95d208fe9641988ba56a2c4f3150d307e43ef2b
Kalyan <kalyanray2004@gmail.com>
2023-05-08 14:18:14 +0530

    Add more javas

---
+++ Decision control instructions/Leap_yr.java
@@ -0,0 +1,17 @@
+//to check where it is leap year or not 
+import java.util.Scanner;
+class leap_yr{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the year : ");
+        int yr = sc.nextInt(); 
+        sc.close();
+        if (yr % 4 == 0 || yr%400 ==0){
+            System.out.println(yr+ " it is a leap year ");
+        }
+        else {
+            System.out.println(yr+" it is not a leap year ");
+        }
+
+    }
+}
